72ccacb509dc76694b004c8515730d4d98b7813a
niteshnigam <niteshnigam10@gmail.com>
2019-11-28 12:58:45 +0530

    Deleted old files

--- README.md
+++
@@ -1,2 +0,0 @@
-# bookmanagement
-bookmanagement
--- fullstack/models/bookgenre_model.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-
-var bookGenreSchema = new mongoose.Schema({
-
-    name: { type: String, require: true, unique: true },
-
-})
-module.exports = mongoose.model('bookGenres', bookGenreSchema);
